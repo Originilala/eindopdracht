8156e85bbf2f6b51f7a8140a78d54cd7e9ed4629
originilala <jonathan.dewsaran@gmail.com>
2022-07-19 22:00:21 +0200

    remove other content from about page

--- src/functions/createRecipePage.js
+++ src/functions/createRecipePage.js
@@ -1,6 +1,7 @@
 import searchImage from "../assets/icons/search.png"
 
 const prepMethod = document.getElementById('main-recipe-site');
+const aboutPage = document.getElementById('main-recipe');
 const ingredientList = document.getElementById('main-recipe-ingredients');
 const nutritionList = document.getElementById('main-recipe-nutrients');
 const labelList = document.getElementById('health-labels');
@@ -39,15 +40,20 @@ export const createRecipePage = (dataRecipe) => {
 }
 
 export const createRecipePage2 = () => {
-    prepMethod.innerHTML = `
-        
-        <div><h3>Webdevelopment Bootcamp Project for Novi Hogeschool</h3><p>-by Originilala.
-        <em>This website has been created as a final assignment for a Webdevelopment bootcamp. The website uses the edamam API from Edamam.com for the retrieval of recipes and the nutrition info.
-        On the home page you can search for recipes by category.
-        There is a handy calculator with which products can be searched by means of the barcode (upc).
-        Then you calculate the nutritional value per portion. Use numbers with decimal points to divide the portion.</em></p>
-        <div id="edamam-badge" data-color="white"></div></div>
-        <img src="${searchImage}" alt="search image" id="search-logo">
+    aboutPage.innerHTML = `
+        <article id="main-recipe-site">
+            <div>
+                <h3>Webdevelopment Bootcamp Project for Novi Hogeschool</h3>
+                <p>-by Originilala.
+                    <em>This website has been created as a final assignment for a Webdevelopment bootcamp. The website uses the edamam API from Edamam.com for the retrieval of recipes and the nutrition info.
+                    On the home page you can search for recipes by category.
+                    There is a handy calculator with which products can be searched by means of the barcode (upc).
+                    Then you calculate the nutritional value per portion. Use numbers with decimal points to divide the portion.</em>
+                </p>
+                <div id="edamam-badge" data-color="white"></div>
+            </div>
+            <img src="${searchImage}" alt="search image" id="search-logo">
+        </article>
     `
     ;
 };
